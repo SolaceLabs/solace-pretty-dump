2ed59c964766db1523aea195f4af6bd588f5fe13
aaron-613 <github@aaronlee.ca>
2024-07-29 03:33:41 -0700

    semp script

---
+++ show-last-nth-msg-spool-id.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# leave these values hardcode here, or make empty to have prompts
+HOST="http://localhost:8080"
+USER="admin"
+PW="admin"
+VPN="default"
+S_QUEUE=""
+NUM=0
+
+echo "┌ This utility returns the Message Spool ID of a message from the back of ┐"
+echo "│ a queue, the nth-newest message.  This ID can then be used with a queue │"
+echo "│ browser to skip all the messages until then. Requires curl and xmllint. │"
+echo "└─────────────────────────────────────────────────────────────────────────┘"
+
+# read in some vars, or hardcode them above
+if [[ $HOST == "" ]]; then
+    echo -n "SEMP URL (e.g. https://aaron.messaging.solace.cloud:943): "
+    read HOST
+fi
+if [[ $USER == "" ]]; then
+    echo -n "SEMP username: "
+    read USER
+fi
+if [[ $PW == "" ]]; then
+    echo -n "SEMP password: "
+    stty -echo
+    read PW
+    stty echo
+    echo
+fi
+if [[ $VPN == "" ]]; then
+    echo -n "Message VPN: "
+    read VPN
+fi
+if [[ $S_QUEUE == "" ]]; then
+    echo -n "Source Queue: "
+    read S_QUEUE
+fi
+if [[ $NUM == 0 ]]; then
+    echo -n "Get MsgSpoolID for nth-last message: n="
+    read NUM
+fi
+
+# here we go, try to get message details...
+
+RGMIDs=`curl -s --compressed -u $USER:$PW $HOST/SEMP -d "<rpc><show><queue><name>$S_QUEUE</name><vpn-name>$VPN</vpn-name><messages/><newest/><count/><num-elements>$NUM</num-elements></queue></show></rpc>" | xmllint --xpath '//message-id/text()' - `
+
+#echo $RGMIDs
+
+if [[ $RGMIDs == "" ]]; then
+    echo "## Problem with SEMP request, probably too many messages! Try less than 28000."
+    echo "## (or modify this script to support paging via <more-cookie>)"
+    exit 1
+fi
+
+IFS=$'\n'
+names=($RGMIDs)
+
+echo
+echo "The MsgSpoolID of the ${#names[@]}th-last message on queue '$S_QUEUE' is: ${names[${#names[@]}-1]} "
